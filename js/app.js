209926ab21ab04301123b7cdda3db8804521f30d
kkinge79 <kingerykagan@gmail.com>
2022-02-03 15:57:53 -0600

    no jquery

--- js/app.js
+++ js/app.js
@@ -1,19 +1,5 @@
 const splash = document.querySelector('.splash');
 
-/* FadeIn Scroll */
-$(document).ready(function() {
-  $(window).scroll( function(){
-      $('.fade').each( function(i){
-          
-          var bottom_of_object = $(this).position().top + $(this).outerHeight();
-          var bottom_of_window = $(window).scrollTop() + $(window).height();
-          if( bottom_of_window > bottom_of_object ){
-              $(this).animate({'opacity':'1'},900);
-          }
-      });
-  });
-});
-
 document.addEventListener('DOMContentLoaded', (e) => {
   setTimeout(( )=> {
     splash.classList.add('display-none')
