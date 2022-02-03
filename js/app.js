231942a0a66c82f078ba648c208a38bb7f6d7022
kkinge79 <kingerykagan@gmail.com>
2022-02-03 15:39:00 -0600

    changed from 100 - 900

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
           var bottom_of_object = $(this).position().top + $(this).outerHeight();
           var bottom_of_window = $(window).scrollTop() + $(window).height();
           if( bottom_of_window > bottom_of_object ){
-              $(this).animate({'opacity':'1'},100);
+              $(this).animate({'opacity':'1'},900);
           }
       });
   });
